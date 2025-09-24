cd8418b9fabb74de562db9c89dd4d0a68a3d463a
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-24 22:35:22 +0200

    new form input component

---
+++ components/form-input.tsx
@@ -0,0 +1,87 @@
+import ThemedText from '@/components/themed-text'
+import { useThemeColor } from '@/hooks/use-theme-color'
+import { useState } from 'react'
+import {
+    KeyboardTypeOptions,
+    Pressable,
+    StyleSheet,
+    TextInput,
+    View,
+} from 'react-native'
+
+type FormInputProps = {
+    handleSubmitEditing?: () => void
+    isPassword?: boolean
+    label: string
+    placeholder: string
+    ref: React.RefObject<TextInput | null>
+    returnKeyType: 'done' | 'next' | 'send'
+    submitBehavior: 'submit' | 'blurAndSubmit'
+    type: KeyboardTypeOptions
+}
+
+const FormInput = ({
+    handleSubmitEditing,
+    isPassword = false,
+    label,
+    placeholder,
+    ref,
+    returnKeyType,
+    submitBehavior,
+    type,
+}: FormInputProps) => {
+    const [isFocused, setIsFocused] = useState(false)
+
+    const borderColorActive = useThemeColor({}, 'border')
+    const borderColorInactive = useThemeColor({}, 'textInactive')
+    const colorRed = useThemeColor({}, 'red')
+    const colorInput = useThemeColor({}, 'text')
+    const colorPlaceholder = useThemeColor({}, 'textInactive')
+
+    const handleFocus = () => {
+        ref?.current?.focus()
+    }
+
+    const styles = StyleSheet.create({
+        button: { width: '100%' },
+        input: {
+            borderColor: isFocused ? borderColorActive : borderColorInactive,
+            borderRadius: 8,
+            borderWidth: 2,
+            color: colorInput,
+            marginVertical: 8,
+            padding: 8,
+        },
+    })
+
+    return (
+        <View>
+            <ThemedText>
+                <Pressable onPress={handleFocus} style={styles.button}>
+                    <ThemedText>
+                        {label}{' '}
+                        <ThemedText style={{ color: colorRed }}>*</ThemedText>
+                    </ThemedText>
+                </Pressable>
+            </ThemedText>
+            <TextInput
+                style={styles.input}
+                placeholder={placeholder}
+                keyboardType={type}
+                autoCapitalize="none"
+                autoCorrect={false}
+                accessibilityLabel={label}
+                secureTextEntry={isPassword}
+                placeholderTextColor={colorPlaceholder}
+                ref={ref}
+                returnKeyType={returnKeyType}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                onSubmitEditing={handleSubmitEditing}
+                submitBehavior={submitBehavior}
+            />
+        </View>
+    )
+}
+
+export default FormInput
--- components/form.tsx
+++ components/form.tsx
@@ -1,10 +1,16 @@
+import FormInput from '@/components/form-input'
 import FormSwitch from '@/components/form-switch'
 import ThemedText from '@/components/themed-text'
 import { useThemeColor } from '@/hooks/use-theme-color'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import { TextInput } from 'react-native'
 
 const Form = () => {
     const [isSigningUp, setIsSigningUp] = useState<boolean>(true)
+
+    const userNameRef = useRef<TextInput>(null)
+    const passwordRef = useRef<TextInput>(null)
+    const confirmPasswordRef = useRef<TextInput>(null)
 
     const color = useThemeColor({}, 'red')
 
@@ -22,6 +28,40 @@ const Form = () => {
                 <ThemedText style={{ color }}>*</ThemedText> are required.
             </ThemedText>
             <FormSwitch handleClick={handleClick} isSigningUp={isSigningUp} />
+            <FormInput
+                handleSubmitEditing={() => passwordRef?.current?.focus()}
+                label="User Name"
+                placeholder="Michael"
+                ref={userNameRef}
+                returnKeyType="next"
+                submitBehavior="submit"
+                type="default"
+            />
+            <FormInput
+                handleSubmitEditing={
+                    isSigningUp
+                        ? () => confirmPasswordRef?.current?.focus()
+                        : undefined
+                }
+                isPassword
+                label="Password"
+                placeholder="password"
+                ref={passwordRef}
+                returnKeyType={isSigningUp ? 'next' : 'done'}
+                submitBehavior={isSigningUp ? 'submit' : 'blurAndSubmit'}
+                type="default"
+            />
+            {isSigningUp && (
+                <FormInput
+                    isPassword
+                    label="Confirm Password"
+                    placeholder="password"
+                    ref={confirmPasswordRef}
+                    returnKeyType="done"
+                    submitBehavior="blurAndSubmit"
+                    type="default"
+                />
+            )}
         </>
     )
 }
