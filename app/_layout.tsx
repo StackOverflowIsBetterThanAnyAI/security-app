d684fcab115c36f512e37cdddbc56b41cffdd05e
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-25 15:53:08 +0200

    preload icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
+import { useColorScheme } from '@/hooks/use-color-scheme'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import {
     DarkTheme,
     DefaultTheme,
     ThemeProvider,
 } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { ActivityIndicator, View } from 'react-native'
 import 'react-native-reanimated'
-
-import { useColorScheme } from '@/hooks/use-color-scheme'
 
 export const unstable_settings = {
     anchor: '(tabs)',
@@ -15,6 +17,24 @@ export const unstable_settings = {
 
 const RootLayout = () => {
     const colorScheme = useColorScheme()
+
+    const [fontsLoaded] = useFonts({
+        ...MaterialCommunityIcons.font,
+    })
+
+    if (!fontsLoaded) {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <ActivityIndicator size="large" />
+            </View>
+        )
+    }
 
     return (
         <ThemeProvider
