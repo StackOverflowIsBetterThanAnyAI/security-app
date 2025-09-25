3ad1daad19d8cb6fd47022f12cd4e977fbbeaba8
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-25 17:22:17 +0200

    remove /ui subfolder

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from 'expo-router'
 
 import HapticTab from '@/components/haptic-tab'
-import IconSymbol, { IconMapping } from '@/components/ui/icon-symbol'
+import IconSymbol, { IconMapping } from '@/components/icon-symbol'
 import { Colors } from '@/constants/theme'
 import { useColorScheme } from '@/hooks/use-color-scheme'
 
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,12 +1,12 @@
 import { Image } from 'expo-image'
 import { StyleSheet } from 'react-native'
 
+import Collapsible from '@/components/collapsible'
 import ExternalLink from '@/components/external-link'
+import IconSymbol from '@/components/icon-symbol'
 import ParallaxScrollView from '@/components/parallax-scroll-view'
 import ThemedText from '@/components/themed-text'
 import ThemedView from '@/components/themed-view'
-import Collapsible from '@/components/ui/collapsible'
-import IconSymbol from '@/components/ui/icon-symbol'
 import { Fonts } from '@/constants/theme'
 
 const ExploreScreen = () => {
