1f8c5420cb0af709e79d60aa3e2c31c27f727512
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-02 20:27:41 +0200

    new user view to logout etc in the future

---
+++ app/(tabs)/user.tsx
@@ -0,0 +1,12 @@
+import MainView from '@/components/main-view'
+import ThemedText from '@/components/themed-text'
+
+const UserScreen = () => {
+    return (
+        <MainView>
+            <ThemedText>User</ThemedText>
+        </MainView>
+    )
+}
+
+export default UserScreen
