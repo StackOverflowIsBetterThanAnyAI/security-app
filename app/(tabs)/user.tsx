2c91bea066987aada8d78dc10d4d4a854a44e4ed
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-06 22:29:55 +0200

    set user is logged in to false

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -3,12 +3,21 @@ import { Pressable } from 'react-native'
 
 import MainView from '@/components/main-view'
 import ThemedText from '@/components/themed-text'
+import { ContextUser } from '@/context/ContextUser'
 import { clearData } from '@/helper/storeData'
+import { useContext } from 'react'
 
 const UserScreen = () => {
+    const contextUser = useContext(ContextUser)
+    if (!contextUser) {
+        throw new Error('UserScreen must be used within a ContextUser.Provider')
+    }
+    const { setIsUserLoggedIn } = contextUser
+
     const router = useRouter()
 
     const handleLogout = () => {
+        setIsUserLoggedIn(false)
         clearData(['authToken', 'authRole', 'authName'])
         router.replace('/login')
     }
