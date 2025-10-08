e905e286420089a726c32da9f3f34f069bd3bcec
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-08 18:40:18 +0200

    outsource pagination button

---
+++ components/pagination-button.tsx
@@ -0,0 +1,74 @@
+import { Pressable, StyleSheet } from 'react-native'
+
+import IconSymbol from '@/components/icon-symbol'
+import { useThemeColor } from '@/hooks/use-theme-color'
+
+type PaginationButtonProps = {
+    handlePress: () => void
+    isDisabled: boolean
+    isLoading: boolean
+    isNext?: boolean
+}
+
+const PaginationButton = ({
+    handlePress,
+    isDisabled,
+    isLoading,
+    isNext = false,
+}: PaginationButtonProps) => {
+    const backgroundColorActive = useThemeColor({}, 'red')
+    const backgroundColorInactive = useThemeColor({}, 'background')
+    const borderColorActive = useThemeColor({}, 'border')
+    const borderColorInactive = useThemeColor({}, 'textInactive')
+    const colorActive = useThemeColor({}, 'tint')
+    const colorInative = useThemeColor({}, 'textInactive')
+
+    const styles = StyleSheet.create({
+        button: {
+            alignItems: 'center',
+            borderRadius: 24,
+            borderWidth: 2,
+            display: 'flex',
+            height: 48,
+            justifyContent: 'center',
+            width: 48,
+        },
+        disabled: {
+            backgroundColor: backgroundColorInactive,
+            borderColor: borderColorInactive,
+        },
+        enabled: {
+            backgroundColor: backgroundColorActive,
+            borderColor: borderColorActive,
+        },
+    })
+
+    return (
+        <Pressable
+            accessible={true}
+            accessibilityRole="button"
+            accessibilityState={{ disabled: isDisabled || isLoading }}
+            accessibilityLabel={`Go to ${isNext ? 'next' : 'previous'} Page${
+                isDisabled || isLoading ? ', disabled' : ''
+            }`}
+            style={({ pressed }) => [
+                styles.button,
+                isDisabled || isLoading ? styles.disabled : styles.enabled,
+                !isDisabled &&
+                    !isLoading &&
+                    pressed && {
+                        opacity: 0.75,
+                    },
+            ]}
+            onPress={!isDisabled && !isLoading ? handlePress : undefined}
+        >
+            <IconSymbol
+                size={28}
+                name={isNext ? 'chevron-right' : 'chevron-left'}
+                color={isDisabled || isLoading ? colorInative : colorActive}
+            />
+        </Pressable>
+    )
+}
+
+export default PaginationButton
