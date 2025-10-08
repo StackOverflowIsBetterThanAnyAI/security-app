889f062786279c1ee786484268549fe933d697fe
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-08 18:40:53 +0200

    use own loading state, apply same state for both buttons, disable them during loading

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,9 +1,9 @@
 import { Router } from 'expo-router'
 import { useContext, useState } from 'react'
-import { Pressable, StyleSheet, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 import { handleApiFetchImages } from '@/api/handleApiFetchImages'
-import IconSymbol from '@/components/icon-symbol'
+import PaginationButton from '@/components/pagination-button'
 import ThemedText from '@/components/themed-text'
 import { ContextError } from '@/context/ContextError'
 import { useThemeColor } from '@/hooks/use-theme-color'
@@ -13,7 +13,6 @@ type PaginationProps = {
     page: number
     router: Router
     setImages: React.Dispatch<React.SetStateAction<string[] | null>>
-    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
     setPage: React.Dispatch<React.SetStateAction<number>>
     setTotalImages: React.Dispatch<React.SetStateAction<number>>
     totalImages: number
@@ -27,7 +26,6 @@ const Pagination = ({
     router,
     setPage,
     setImages,
-    setIsLoading,
     setTotalImages,
     totalImages,
     url,
@@ -41,13 +39,9 @@ const Pagination = ({
     }
     const { setError, setRetryFn } = contextError
 
-    const backgroundColorActive = useThemeColor({}, 'red')
     const backgroundColorInactive = useThemeColor({}, 'background')
-    const borderColorActive = useThemeColor({}, 'border')
-    const borderColorInactive = useThemeColor({}, 'textInactive')
-    const colorActive = useThemeColor({}, 'tint')
-    const colorInative = useThemeColor({}, 'textInactive')
 
+    const [isLoading, setIsLoading] = useState<boolean>(false)
     const [isNextDisabled, setIsNextDisabled] = useState<boolean>(false)
     const [isPreviousDisabled, setIsPreviousDisabled] = useState<boolean>(true)
 
@@ -97,23 +91,6 @@ const Pagination = ({
     }
 
     const styles = StyleSheet.create({
-        button: {
-            alignItems: 'center',
-            borderRadius: 24,
-            borderWidth: 2,
-            display: 'flex',
-            height: 48,
-            justifyContent: 'center',
-            width: 48,
-        },
-        disabled: {
-            backgroundColor: backgroundColorInactive,
-            borderColor: borderColorInactive,
-        },
-        enabled: {
-            backgroundColor: backgroundColorActive,
-            borderColor: borderColorActive,
-        },
         wrapper: {
             alignItems: 'center',
             backgroundColor: backgroundColorInactive,
@@ -130,53 +107,19 @@ const Pagination = ({
 
     return (
         <View style={styles.wrapper}>
-            <Pressable
-                accessible={true}
-                accessibilityRole="button"
-                accessibilityState={{ disabled: isPreviousDisabled }}
-                accessibilityLabel={`Go to previous Page${
-                    isPreviousDisabled ? ', disabled' : ''
-                }`}
-                style={({ pressed }) => [
-                    styles.button,
-                    isPreviousDisabled ? styles.disabled : styles.enabled,
-                    !isPreviousDisabled &&
-                        pressed && {
-                            opacity: 0.75,
-                        },
-                ]}
-                onPress={!isPreviousDisabled ? handlePressPrevious : undefined}
-            >
-                <IconSymbol
-                    size={28}
-                    name={'chevron-left'}
-                    color={isPreviousDisabled ? colorInative : colorActive}
-                />
-            </Pressable>
+            <PaginationButton
+                handlePress={handlePressPrevious}
+                isDisabled={isPreviousDisabled}
+                isLoading={isLoading}
+                isNext={false}
+            />
             <ThemedText>{page}</ThemedText>
-            <Pressable
-                accessible={true}
-                accessibilityRole="button"
-                accessibilityState={{ disabled: isNextDisabled }}
-                accessibilityLabel={`Go to next Page${
-                    isNextDisabled ? ', disabled' : ''
-                }`}
-                style={({ pressed }) => [
-                    styles.button,
-                    isNextDisabled ? styles.disabled : styles.enabled,
-                    !isNextDisabled &&
-                        pressed && {
-                            opacity: 0.75,
-                        },
-                ]}
-                onPress={!isNextDisabled ? handlePressNext : undefined}
-            >
-                <IconSymbol
-                    size={28}
-                    name={'chevron-right'}
-                    color={isNextDisabled ? colorInative : colorActive}
-                />
-            </Pressable>
+            <PaginationButton
+                handlePress={handlePressNext}
+                isDisabled={isNextDisabled}
+                isLoading={isLoading}
+                isNext={true}
+            />
         </View>
     )
 }
