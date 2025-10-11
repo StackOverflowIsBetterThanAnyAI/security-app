8e633881f7d01faa6cc2fc85cd26a2aceea8656c
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-11 22:09:49 +0200

    user grid component

---
+++ components/user-grid.tsx
@@ -0,0 +1,24 @@
+import { FlatList } from 'react-native'
+
+import { UsersType } from '@/api/handleApiFetchUsers'
+import UserGridItem from './user-grid-item'
+
+type UserGridProps = {
+    users: UsersType[]
+}
+
+const UserGrid = ({ users }: UserGridProps) => {
+    return (
+        <FlatList
+            data={users}
+            keyExtractor={(item) => item.id.toString()}
+            scrollEnabled={false}
+            style={{ paddingTop: 8 }}
+            renderItem={({ item, index }) => (
+                <UserGridItem item={item} index={index} />
+            )}
+        />
+    )
+}
+
+export default UserGrid
