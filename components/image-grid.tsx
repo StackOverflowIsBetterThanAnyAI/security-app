3257c0db6664536686a098d16888b1a0df4cbd36
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-11 20:34:44 +0200

    add key

--- components/image-grid.tsx
+++ components/image-grid.tsx
@@ -24,6 +24,7 @@ const ImageGrid = ({
             scrollEnabled={false}
             renderItem={({ item, index }) => (
                 <ImageGridItem
+                    key={item}
                     bottomRowIndices={[ITEMS_PER_PAGE - 1, ITEMS_PER_PAGE - 2]}
                     index={index}
                     item={item}
