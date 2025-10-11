6e823647b5c24fa6b5b76c219ce7899289a8d70d
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-11 20:34:53 +0200

    fetch users

---
+++ api/handleApiFetchUsers.ts
@@ -0,0 +1,82 @@
+import { UserRoleType } from '@/context/ContextUser'
+import { clearData } from '@/helper/storeData'
+import { Router } from 'expo-router'
+
+export type UsersType = {
+    id: number
+    name: string
+    role: UserRoleType
+}
+
+type handleApiFetchUsersProps = {
+    router: Router
+    setError: (value: React.SetStateAction<string>) => void
+    setUsers: (value: React.SetStateAction<UsersType[]>) => void
+    setIsLoading: (value: React.SetStateAction<boolean>) => void
+    setIsUserLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
+    setRetryFn: React.Dispatch<React.SetStateAction<(() => void) | null>>
+    url: string
+    userToken: string
+}
+
+export const handleApiFetchUsers = async ({
+    router,
+    setError,
+    setUsers,
+    setIsLoading,
+    setIsUserLoggedIn,
+    setRetryFn,
+    url,
+    userToken,
+}: handleApiFetchUsersProps) => {
+    setIsLoading(true)
+    try {
+        const response = await fetch(`${url}/users`, {
+            headers: {
+                Authorization: `Bearer ${userToken}`,
+            },
+        })
+
+        if (!response.ok) {
+            if (response.status === 403) {
+                setIsUserLoggedIn(false)
+                setError('')
+                await clearData(['authToken', 'authRole', 'authName'])
+                router.replace('/(tabs)/login')
+                return
+            }
+
+            const data = await response.json().catch(() => ({}))
+            const message = data.error || response.statusText || 'Unknown error'
+            throw new Error(`Error ${response.status}: ${message}`)
+        }
+        const users: UsersType[] = await response.json()
+        setUsers(users)
+
+        setError('')
+        setRetryFn(() => {})
+    } catch (error: any) {
+        setError(error.message)
+
+        queueMicrotask(() => {
+            setRetryFn(() => () => {
+                handleApiFetchUsers({
+                    router,
+                    setError,
+                    setUsers,
+                    setIsLoading,
+                    setIsUserLoggedIn,
+                    setRetryFn,
+                    url,
+                    userToken,
+                })
+            })
+            router.push({
+                pathname: '/(tabs)/error',
+                params: { from: 'admin' },
+            })
+        })
+    } finally {
+        setIsLoading(false)
+    }
+}
