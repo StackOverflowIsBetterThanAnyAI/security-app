919ee9c2b9e0c759df6b6dd5a5233453b09d4a9f
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-12 12:15:47 +0200

    admin control panel

--- app/(tabs)/admin.tsx
+++ app/(tabs)/admin.tsx
@@ -1,29 +1,66 @@
 import { useRouter } from 'expo-router'
-import { useContext } from 'react'
-import { Pressable, StyleSheet, View } from 'react-native'
+import { useContext, useEffect, useState } from 'react'
+import {
+    ActivityIndicator,
+    Platform,
+    Pressable,
+    StyleSheet,
+    View,
+} from 'react-native'
 
+import { handleApiFetchUsers, UsersType } from '@/api/handleApiFetchUsers'
 import Button from '@/components/button'
-import IconSymbol from '@/components/icon-symbol'
+import IconSymbol, { noUsers } from '@/components/icon-symbol'
 import MainView from '@/components/main-view'
 import ThemedText from '@/components/themed-text'
+import UserGrid from '@/components/user-grid'
+import { URL, URL_MOBILE } from '@/constants/api'
+import { ContextError } from '@/context/ContextError'
 import { ContextUser } from '@/context/ContextUser'
 import { clearData } from '@/helper/storeData'
 import { useThemeColor } from '@/hooks/use-theme-color'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 
 const AdminScreen = () => {
+    const contextError = useContext(ContextError)
+    if (!contextError) {
+        throw new Error(
+            'AdminScreen must be used within a ContextError.Provider'
+        )
+    }
+    const { setError, setRetryFn } = contextError
+
     const contextUser = useContext(ContextUser)
     if (!contextUser) {
-        throw new Error('UserScreen must be used within a ContextUser.Provider')
+        throw new Error(
+            'AdminScreen must be used within a ContextUser.Provider'
+        )
     }
-    const { setIsUserLoggedIn, userName } = contextUser
+    const { setIsUserLoggedIn, userName, userToken } = contextUser
 
-    const avatarColor = useThemeColor({}, 'text')
+    const avatarColor = useThemeColor({}, 'textLight')
     const backgroundColorAvatar = useThemeColor({}, 'buttonInactive')
     const backgroundColorButton = useThemeColor({}, 'background')
     const borderColorButton = useThemeColor({}, 'border')
+    const colorIcon = useThemeColor({}, 'text')
     const router = useRouter()
+    const tabBarHeight = useBottomTabBarHeight()
 
-    const handleFetchUsers = () => {}
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [users, setUsers] = useState<UsersType[]>([])
+
+    const handleFetchUsers = () => {
+        handleApiFetchUsers({
+            router,
+            setError,
+            setIsLoading,
+            setIsUserLoggedIn,
+            setRetryFn,
+            setUsers,
+            url: Platform.OS === 'web' ? URL : URL_MOBILE,
+            userToken,
+        })
+    }
 
     const handleLogout = () => {
         setIsUserLoggedIn(false)
@@ -31,7 +68,20 @@ const AdminScreen = () => {
         router.replace('/login')
     }
 
+    useEffect(() => {
+        handleFetchUsers()
+    }, [])
+
     const styles = StyleSheet.create({
+        activityLoader: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 10,
+        },
         avatarContainer: {
             alignSelf: 'center',
             backgroundColor: backgroundColorAvatar,
@@ -50,6 +100,11 @@ const AdminScreen = () => {
             minWidth: 144,
             paddingVertical: 8,
             paddingHorizontal: 24,
+        },
+        noUsersContainer: {
+            gap: 16,
+            alignItems: 'center',
+            marginTop: 24,
         },
         titleContainer: {
             flexDirection: 'column',
@@ -79,11 +134,34 @@ const AdminScreen = () => {
                     Role: Admin
                 </ThemedText>
             </View>
-            <Button
-                accessibilityLabel="Refresh Users"
-                handlePress={handleFetchUsers}
-                label="Refresh"
-            />
+            {isLoading && !users ? (
+                <View
+                    style={[styles.activityLoader, { bottom: -tabBarHeight }]}
+                >
+                    <ActivityIndicator size="large" color={colorIcon} />
+                </View>
+            ) : users.length ? (
+                <>
+                    <Button
+                        accessibilityLabel="Refresh Users"
+                        handlePress={handleFetchUsers}
+                        label="Refresh"
+                    />
+                    <UserGrid setUsers={setUsers} users={users} />
+                </>
+            ) : (
+                <View style={styles.noUsersContainer}>
+                    {noUsers(colorIcon)}
+                    <ThemedText center>
+                        Currently, there are no other Users registered.
+                    </ThemedText>
+                    <Button
+                        accessibilityLabel="Refresh Users"
+                        handlePress={handleFetchUsers}
+                        label="Refresh"
+                    />
+                </View>
+            )}
             <Pressable
                 onPress={handleLogout}
                 accessible={true}
