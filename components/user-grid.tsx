b521a3948a97a38151757fc85e9695ec09dadeb2
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-12 13:59:28 +0200

    additional horizontal line

--- components/user-grid.tsx
+++ components/user-grid.tsx
@@ -2,6 +2,7 @@ import { FlatList } from 'react-native'
 
 import { UsersType } from '@/api/handleApiFetchUsers'
 import UserGridItem from '@/components/user-grid-item'
+import { useThemeColor } from '@/hooks/use-theme-color'
 
 type UserGridProps = {
     setUsers: React.Dispatch<React.SetStateAction<UsersType[]>>
@@ -9,12 +10,19 @@ type UserGridProps = {
 }
 
 const UserGrid = ({ setUsers, users }: UserGridProps) => {
+    const borderColor = useThemeColor({}, 'border')
+
     return (
         <FlatList
             data={users}
             keyExtractor={(item) => item.id.toString()}
             scrollEnabled={false}
-            style={{ paddingTop: 8 }}
+            style={{
+                paddingVertical: 8,
+                borderBottomWidth: 2,
+                borderBottomColor: borderColor,
+                marginHorizontal: 8,
+            }}
             renderItem={({ item }) => (
                 <UserGridItem item={item} setUsers={setUsers} />
             )}
