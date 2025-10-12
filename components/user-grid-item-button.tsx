12f518c01a74bac3ab505350858e0de15a86624b
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-12 12:16:20 +0200

    user grid item / button components

---
+++ components/user-grid-item-button.tsx
@@ -0,0 +1,102 @@
+import { ActivityIndicator, Pressable, StyleSheet } from 'react-native'
+
+import ThemedText from '@/components/themed-text'
+import { UserRoleType } from '@/context/ContextUser'
+import { useThemeColor } from '@/hooks/use-theme-color'
+
+type UserGridItemButtonProps = {
+    isDelete?: boolean
+    isLoading: boolean
+    handlePress: () => void
+    role: UserRoleType
+}
+
+const UserGridItemButton = ({
+    isDelete = false,
+    isLoading,
+    handlePress,
+    role,
+}: UserGridItemButtonProps) => {
+    const activityColor = useThemeColor({}, 'text')
+    const backgroundColor = useThemeColor({}, 'buttonInactive')
+    const borderColorActive = useThemeColor({}, 'border')
+    const borderColorInactive = useThemeColor({}, 'textInactive')
+    const borderColorRedActive = useThemeColor({}, 'red')
+    const borderColorRedInactive = useThemeColor({}, 'redInactive')
+
+    const styles = StyleSheet.create({
+        button: {
+            backgroundColor,
+            borderRadius: 12,
+            borderWidth: 2,
+            minWidth: 144,
+            paddingVertical: 4,
+            paddingHorizontal: 16,
+        },
+        disabledDelete: {
+            borderColor: borderColorRedInactive,
+        },
+        disabledRole: {
+            borderColor: borderColorInactive,
+        },
+        enabledDelete: {
+            borderColor: borderColorRedActive,
+        },
+        enabledRole: {
+            borderColor: borderColorActive,
+        },
+    })
+
+    return (
+        <Pressable
+            accessible={true}
+            accessibilityRole="button"
+            accessibilityState={{
+                disabled: isLoading || role === 'admin',
+            }}
+            accessibilityLabel={`${
+                isDelete
+                    ? 'Delete User'
+                    : role === 'user'
+                    ? 'Promote'
+                    : 'Demote'
+            }${isLoading || role === 'admin' ? ', disabled' : ''}`}
+            onPress={isLoading || role !== 'admin' ? handlePress : undefined}
+            style={({ pressed }) => [
+                {
+                    opacity:
+                        pressed && !isLoading && role !== 'admin' ? 0.75 : 1,
+                },
+                styles.button,
+                isDelete
+                    ? isLoading || role === 'admin'
+                        ? styles.disabledDelete
+                        : styles.enabledDelete
+                    : isLoading || role === 'admin'
+                    ? styles.disabledRole
+                    : styles.enabledRole,
+            ]}
+        >
+            {isLoading ? (
+                <ActivityIndicator
+                    color={activityColor}
+                    style={{ height: 26 }}
+                />
+            ) : (
+                <ThemedText
+                    center
+                    isDisabled={role === 'admin'}
+                    style={{ height: 26 }}
+                >
+                    {isDelete
+                        ? 'Delete User'
+                        : role === 'user'
+                        ? 'Promote'
+                        : 'Demote'}
+                </ThemedText>
+            )}
+        </Pressable>
+    )
+}
+
+export default UserGridItemButton
---
+++ components/user-grid-item.tsx
@@ -0,0 +1,116 @@
+import { useRouter } from 'expo-router'
+import { useContext, useState } from 'react'
+import { Platform, StyleSheet, View } from 'react-native'
+
+import { handleApiChangeRole } from '@/api/handleApiChangeRole'
+import { handleApiDeleteUser } from '@/api/handleApiDeleteUser'
+import { UsersType } from '@/api/handleApiFetchUsers'
+import ThemedText from '@/components/themed-text'
+import UserGridItemButton from '@/components/user-grid-item-button'
+import { URL, URL_MOBILE } from '@/constants/api'
+import { ContextError } from '@/context/ContextError'
+import { ContextUser } from '@/context/ContextUser'
+import { useThemeColor } from '@/hooks/use-theme-color'
+
+type UserGridItemProps = {
+    item: UsersType
+    setUsers: React.Dispatch<React.SetStateAction<UsersType[]>>
+}
+
+const UserGridItem = ({ item, setUsers }: UserGridItemProps) => {
+    const contextError = useContext(ContextError)
+    if (!contextError) {
+        throw new Error(
+            'UserGridItem must be used within a ContextError.Provider'
+        )
+    }
+    const { setError, setRetryFn } = contextError
+
+    const contextUser = useContext(ContextUser)
+    if (!contextUser) {
+        throw new Error(
+            'UserGridItem must be used within a ContextUser.Provider'
+        )
+    }
+    const { setIsUserLoggedIn, userToken } = contextUser
+
+    const backgroundColor = useThemeColor({}, 'buttonInactive')
+    const router = useRouter()
+
+    const [isLoadingDelete, setIsLoadingDelete] = useState<boolean>(false)
+    const [isLoadingRole, setIsLoadingRole] = useState<boolean>(false)
+
+    const handleDelete = () => {
+        handleApiDeleteUser({
+            id: item.id,
+            router,
+            setError,
+            setIsLoading: setIsLoadingDelete,
+            setIsUserLoggedIn,
+            setUsers,
+            setRetryFn,
+            url: Platform.OS === 'web' ? URL : URL_MOBILE,
+            userToken,
+        })
+    }
+
+    const handleRole = () => {
+        handleApiChangeRole({
+            id: item.id,
+            role: item.role === 'user' ? 'member' : 'user',
+            router,
+            setError,
+            setIsLoading: setIsLoadingRole,
+            setIsUserLoggedIn,
+            setRetryFn,
+            setUsers,
+            url: Platform.OS === 'web' ? URL : URL_MOBILE,
+            userToken,
+        })
+    }
+
+    const styles = StyleSheet.create({
+        buttonContainer: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+        },
+        textContainer: { alignItems: 'flex-end', flexDirection: 'row', gap: 2 },
+        wrapper: {
+            backgroundColor,
+            borderRadius: 8,
+            flexDirection: 'column',
+            gap: 12,
+            justifyContent: 'space-between',
+            marginBottom: 12,
+            minHeight: 96,
+            paddingHorizontal: 12,
+            paddingVertical: 8,
+        },
+    })
+
+    return (
+        <View style={styles.wrapper}>
+            <View style={styles.textContainer}>
+                <ThemedText type="subtitle" numberOfLines={1}>
+                    {item.name}
+                </ThemedText>
+                <ThemedText>: {item.role}</ThemedText>
+            </View>
+            <View style={styles.buttonContainer}>
+                <UserGridItemButton
+                    handlePress={handleRole}
+                    isLoading={isLoadingRole}
+                    role={item.role}
+                />
+                <UserGridItemButton
+                    handlePress={handleDelete}
+                    isDelete
+                    isLoading={isLoadingDelete}
+                    role={item.role}
+                />
+            </View>
+        </View>
+    )
+}
+
+export default UserGridItem
