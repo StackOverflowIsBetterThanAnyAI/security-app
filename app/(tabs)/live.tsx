5052e67f16facfb287681ae548df5a94c1f1c6bb
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-23 00:33:45 +0200

    integrate live image and form components directly in their parent views to stay uniform with other views

--- app/(tabs)/live.tsx
+++ app/(tabs)/live.tsx
@@ -1,17 +1,154 @@
-import { useContext } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { useFocusEffect, useRouter } from 'expo-router'
+import { useCallback, useContext, useState } from 'react'
+import {
+    ActivityIndicator,
+    Image,
+    Pressable,
+    StyleSheet,
+    View,
+} from 'react-native'
 
-import LiveImage from '@/components/live-image'
+import { handleApiFetchLive } from '@/api/handleApiFetchLive'
+import Button from '@/components/button'
+import { noFile } from '@/components/icon-symbol'
 import MainView from '@/components/main-view'
 import ThemedText from '@/components/themed-text'
+import { URL } from '@/constants/api'
+import { ContextError } from '@/context/ContextError'
 import { ContextUser } from '@/context/ContextUser'
+import { useThemeColor } from '@/hooks/use-theme-color'
+import { downloadImage } from '@/utils/downloadImage'
+
+const errorImageSource = require('./../assets/images/error.webp')
 
 const LiveScreen = () => {
+    const contextError = useContext(ContextError)
+    if (!contextError) {
+        throw new Error(
+            'LiveScreen must be used within a ContextError.Provider'
+        )
+    }
+    const { setError, setRetryFn } = contextError
+
     const contextUser = useContext(ContextUser)
     if (!contextUser) {
         throw new Error('LiveScreen must be used within a ContextUser.Provider')
     }
-    const { userName } = contextUser
+    const { setIsUserLoggedIn, userName, userToken } = contextUser
+
+    const [refreshTime, setRefreshTime] = useState<number | null>(Date.now())
+    const [imageIsLoaded, setImageIsLoaded] = useState<boolean>(false)
+    const [imageLoadFailed, setImageLoadFailed] = useState(false)
+    const [imageName, setImageName] = useState<string>('')
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+
+    const activityColor = useThemeColor({}, 'text')
+    const backgroundColor = useThemeColor({}, 'background')
+    const backgroundColorSecondary = useThemeColor({}, 'buttonInactive')
+    const colorIcon = useThemeColor({}, 'text')
+    const router = useRouter()
+
+    const fullUriWithToken = `${URL}/live?token=${userToken}&time=${refreshTime}`
+
+    const imageSource = imageLoadFailed
+        ? errorImageSource
+        : { uri: fullUriWithToken }
+
+    const formattedDate = () => {
+        if (
+            !/security_image_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.jpg/.test(
+                imageName
+            )
+        ) {
+            return 'Current Live Image'
+        }
+        const date = imageName
+            .replace(/^(security_image_)/, '')
+            .replace(/(.jpg$)/, '')
+            .split(/_/)
+
+        const day = date[0]
+        const time = date[1].replace(/-/g, ':')
+
+        return `Live Image ${day} ${time}`
+    }
+
+    const handlePress = () => {
+        downloadImage({ imageName, imageSource })
+    }
+
+    const handleFetchLive = useCallback(() => {
+        handleApiFetchLive({
+            router,
+            setError,
+            setImageName,
+            setIsLoading,
+            setIsUserLoggedIn,
+            setRetryFn,
+            userToken,
+        })
+    }, [
+        router,
+        setError,
+        setImageName,
+        setIsLoading,
+        setIsUserLoggedIn,
+        setRetryFn,
+        userToken,
+    ])
+
+    const handleRefreshImage = useCallback(() => {
+        setImageLoadFailed(false)
+        setImageIsLoaded(false)
+        setRefreshTime(Date.now())
+        handleFetchLive()
+    }, [setImageLoadFailed, setImageIsLoaded, setRefreshTime, handleFetchLive])
+
+    useFocusEffect(
+        useCallback(() => {
+            handleRefreshImage()
+            const refresh = setInterval(() => {
+                handleRefreshImage()
+            }, 30000)
+            return () => {
+                clearInterval(refresh)
+            }
+        }, [handleRefreshImage])
+    )
+
+    const styles = StyleSheet.create({
+        footer: {
+            backgroundColor,
+            paddingHorizontal: 16,
+            paddingVertical: 12,
+            width: '100%',
+        },
+        image: {
+            height: '100%',
+            opacity: imageIsLoaded ? 1 : 0,
+            width: '100%',
+        },
+        imageWrapper: {
+            aspectRatio: 16 / 9,
+            backgroundColor,
+        },
+        loadingOverlay: {
+            position: 'absolute',
+            aspectRatio: 16 / 9,
+            backgroundColor: backgroundColorSecondary,
+            justifyContent: 'center',
+            width: '100%',
+        },
+        noImagesContainer: {
+            gap: 16,
+            alignItems: 'center',
+            marginTop: 64,
+        },
+        titleContainer: {
+            flexDirection: 'column',
+            gap: 16,
+        },
+    })
 
     return (
         <MainView>
@@ -21,16 +158,68 @@ const LiveScreen = () => {
                 </ThemedText>
                 <ThemedText type="title">Live Image</ThemedText>
             </View>
-            <LiveImage />
+            {imageName ? (
+                <>
+                    <Button
+                        accessibilityLabel="Refresh Recordings"
+                        handlePress={handleRefreshImage}
+                        label="Refresh"
+                    />
+                    <Pressable
+                        accessible={true}
+                        accessibilityRole="button"
+                        onPress={handlePress}
+                        style={({ pressed }) => [
+                            { opacity: pressed ? 0.75 : 1 },
+                            styles.imageWrapper,
+                        ]}
+                    >
+                        <Image
+                            alt={formattedDate()}
+                            source={imageSource}
+                            style={styles.image}
+                            resizeMode="cover"
+                            onError={() => {
+                                setImageLoadFailed(true)
+                                setImageIsLoaded(true)
+                            }}
+                            onLoad={() => setImageIsLoaded(true)}
+                        />
+                        {imageIsLoaded &&
+                            imageSource.uri &&
+                            imageName &&
+                            !isLoading && (
+                                <View style={styles.footer}>
+                                    <ThemedText center>
+                                        {formattedDate()}
+                                    </ThemedText>
+                                </View>
+                            )}
+                        {!imageIsLoaded && (
+                            <View style={styles.loadingOverlay}>
+                                <ActivityIndicator
+                                    color={activityColor}
+                                    size="large"
+                                />
+                            </View>
+                        )}
+                    </Pressable>
+                </>
+            ) : (
+                <View style={styles.noImagesContainer}>
+                    {noFile(colorIcon)}
+                    <ThemedText center>
+                        Currently, there is no Live Image available.
+                    </ThemedText>
+                    <Button
+                        accessibilityLabel="Refresh Recordings"
+                        handlePress={handleRefreshImage}
+                        label="Refresh"
+                    />
+                </View>
+            )}
         </MainView>
     )
 }
-
-const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: 'column',
-        gap: 16,
-    },
-})
 
 export default LiveScreen
